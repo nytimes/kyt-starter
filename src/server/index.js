4d64915fe49e7930950ea9069afdfea47e3ed13c
Price <carolyn.price@nytimes.com>
2016-09-08 14:27:02 -0400

    WIP first pass

--- src/server/index.js
+++ src/server/index.js
@@ -5,10 +5,15 @@ const path = require('path');
 const clientAssets = require(process.env.ASSETS_MANIFEST);
 
 const app = express();
-app.use(express.static(path.join(process.cwd(), process.env.PUBLIC_DIR)));
-
+const staticPath = path.join(process.cwd(), process.env.PUBLIC_DIR);
+app.use(express.static(staticPath));
+const favicon = "";//path.join(staticPath, '/images/kyt_favicon.png');
 app.get('/', (req, res) => {
   res.send(`
+    <head>
+      <link rel="shortcut icon" href="${favicon}">
+      <link href="${clientAssets.main.css}" rel="stylesheet" type="text/css"/>
+    </head>
     <body>
       <div id='root'></div>
       <script src='${clientAssets.main.js}'></script>
@@ -17,4 +22,3 @@ app.get('/', (req, res) => {
 });
 
 app.listen(parseInt(process.env.SERVER_PORT, 10));
-
